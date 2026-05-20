d02f9cfe8265c381eacc8b6fcec129606590bb36
t1user <tomasz2605@gmail.com>
2026-05-20 12:59:35 +0200

    Fix pandas multi-index sort compatibility [codex]

--- arctic/multi_index.py
+++ arctic/multi_index.py
@@ -16,6 +16,11 @@ logger = logging.getLogger(__name__)
 
 # ----------------------- Grouping and Aggregating  ---------------------------- #
 
+def _lexsort_depth(index):
+    """Return MultiIndex lexsort depth across pandas versions."""
+    return getattr(index, "lexsort_depth", index._lexsort_depth)
+
+
 def fancy_group_by(df, grouping_level=0, aggregate_level=1, method='last', max_=None, min_=None, within=None):
     """ Dataframe group-by operation that supports aggregating by different methods on the index.
 
@@ -64,7 +69,7 @@ def fancy_group_by(df, grouping_level=0, aggregate_level=1, method='last', max_=
     # The sort order must be correct in order of grouping_level -> aggregate_level for the aggregation methods
     # to work properly. We can check the sortdepth to see if this is in fact the case and resort if necessary.
     # TODO: this might need tweaking if the levels are around the wrong way
-    if df.index.lexsort_depth < (aggregate_level + 1):
+    if _lexsort_depth(df.index) < (aggregate_level + 1):
         df = df.sort_index(level=grouping_level)
 
     gb = df.groupby(level=grouping_level)
@@ -109,13 +114,15 @@ def multi_index_insert_row(df, index_row, values_row):
     """
     if PD_VER < '0.24.0':
         row_index = pd.MultiIndex(levels=[[i] for i in index_row],
-                                  labels=[[0] for i in index_row])
+                                  labels=[[0] for i in index_row],
+                                  names=df.index.names)
     else:
         row_index = pd.MultiIndex(levels=[[i] for i in index_row],
-                                  codes=[[0] for i in index_row])
+                                  codes=[[0] for i in index_row],
+                                  names=df.index.names)
     row = pd.DataFrame(values_row, index=row_index, columns=df.columns)
     df = pd.concat((df, row))
-    if df.index.lexsort_depth == len(index_row) and df.index[-2] < df.index[-1]:
+    if _lexsort_depth(df.index) == len(index_row) and df.index[-2] < df.index[-1]:
         # We've just appended a row to an already-sorted dataframe
         return df
     # The df wasn't sorted or the row has to be put in the middle somewhere
